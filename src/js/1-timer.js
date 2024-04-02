df929ee844efe6d8d1bd3f5edfa80b15394f59db
Vitaly112 <149366473+Vitaly112@users.noreply.github.com>
2024-04-02 11:59:57 +0300

    min css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,91 @@
-console.log('tata');
+import flatpickr from "flatpickr"; 
+import "flatpickr/dist/flatpickr.min.css";   
+import iziToast from "izitoast";   
+import "izitoast/dist/css/iziToast.min.css"; 
+
+const startBtn = document.querySelector("[data-start]");;
+const daysRef = document.querySelector('span[data-days]');
+const hoursRef = document.querySelector('span[data-hours]');
+const minutesRef = document.querySelector('span[data-minutes]');
+const secondsRef = document.querySelector('span[data-seconds]');
+const inputDateTime = document.querySelector('#datetime-picker');
+
+startBtn.disabled = true;
+startBtn.style.backgroundColor = "#CFCFCF";
+startBtn.style.color = "#989898";
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0].getTime() < new Date().getTime()) {
+            
+            daysRef.textContent === "00";
+            hoursRef.textContent === "00";
+            minutesRef.textContent === "00";
+            secondsRef.textContent === "00";
+            iziToast.error({
+                title: 'Error',
+                titleColor: 'black',
+                backgroundColor: 'red',
+                message: 'Please choose a date in the future',
+                position: 'topRight',
+            });
+      } else {
+            startBtn.disabled = false;
+            startBtn.style.backgroundColor = "#4E75FF";
+            startBtn.style.color = "#FFFFFF";
+            startBtn.addEventListener("click", () => {    
+                startBtn.style.backgroundColor = "#CFCFCF";
+                startBtn.style.color = "#989898";
+                const userDate = setInterval(() => {
+                    const time = convertMs(selectedDates[0].getTime() - new Date().getTime());
+                    daysRef.textContent = addLeadingZero(time.days);
+                   
+                    hoursRef.textContent = addLeadingZero(time.hours);
+                    minutesRef.textContent = addLeadingZero(time.minutes);
+                    secondsRef.textContent = addLeadingZero(time.seconds);
+                    if (daysRef.textContent === "00" &&
+                        hoursRef.textContent === "00" &&
+                        minutesRef.textContent === "00" &&
+                        secondsRef.textContent === "00") {
+                        console.log(userDate);
+                        clearInterval(userDate);
+                    }
+                }, 1000);
+                startBtn.disabled = true;
+                inputDateTime.disabled = true;
+            })
+  }
+  }
+};
+
+flatpickr(inputDateTime, options);
+
+
+function addLeadingZero(value) {
+   
+    return value.toString().padStart(2, '0');
+}
+
+
+
+
+function convertMs(ms) {
+   
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);   
+    const hours = Math.floor((ms % day) / hour); 
+    const minutes = Math.floor(((ms % day) % hour) / minute); 
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second); 
+
+    return { days, hours, minutes, seconds };
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,59 @@
+import iziToast from "izitoast";   
+import "izitoast/dist/css/iziToast.min.css"; 
+
+const form = document.querySelector(".form"); 
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const { delay, state } = event.currentTarget.elements;
+
+    // Валідація введених даних
+    const delayValue = parseInt(delay.value);
+    if (isNaN(delayValue) || delayValue <= 0) {
+        iziToast.error({
+            title: 'Error',
+            titleColor: 'black',
+            backgroundColor: '#EF4040',
+            color: '#FFFFFF',
+            message: `❌ Please enter a valid positive integer for delay`,
+            position: 'topRight',
+        });
+        return;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state.value === "fulfilled") {
+                resolve();
+            } else {
+                reject();
+            }
+        }, delayValue);
+    });
+
+    // Обробка помилок у промісах
+    promise
+        .then(() => {
+            iziToast.success({
+                title: 'OK',
+                titleColor: 'black',
+                backgroundColor: '#59A10D',
+                color: '#FFFFFF',
+                message: `✅ Fulfilled promise in ${delayValue}ms`,
+                position: 'topRight',
+            });
+        })
+        .catch(() => {
+            iziToast.error({
+                title: 'Error',
+                titleColor: 'black',
+                backgroundColor: '#EF4040',
+                color: '#FFFFFF',
+                message: `❌ Rejected promise in ${delayValue}ms`,
+                position: 'topRight',
+            });
+        });
+}
